e6a39b90bfef2d8dd4bff0f2c372a5af0eb00bb3
Maryasha <marbigar@gmail.com>
2017-12-07 01:04:18 +0200

    Add - ngRouter + templates

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('CoinKeeperApp', []);
+var app = angular.module('CoinKeeperApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "templates/categories.html"
+        })
+        .when("/history", {
+            templateUrl : "templates/history.html"
+        })
+});

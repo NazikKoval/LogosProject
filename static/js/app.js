1741389a0870a1abd1ec3f68ec3a559696d65e8a
Maryasha <marbigar@gmail.com>
2017-12-07 01:17:55 +0200

    Mod - replaced ngRoute with ui-route

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,18 @@
-var app = angular.module('CoinKeeperApp', ['ngRoute']);
+var app = angular.module('CoinKeeperApp', ['ui.router']);
 
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl : "templates/categories.html"
-        })
-        .when("/history", {
-            templateUrl : "templates/history.html"
-        })
+app.config(function($stateProvider) {
+    var categoriesState = {
+        name: 'categories',
+        url: '/',
+        templateUrl: 'templates/categories.html'
+    };
+
+    var historyState = {
+        name: 'history',
+        url: '/history',
+        templateUrl: 'templates/history.html'
+    };
+
+    $stateProvider.state(categoriesState);
+    $stateProvider.state(historyState);
 });

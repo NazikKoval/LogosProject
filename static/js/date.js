3e2a97328d2dcc9209247cfec558f1143ca93e11
Maryasha <marbigar@gmail.com>
2017-12-07 03:50:19 +0200

    Add - moved date & time to controller

--- static/js/date.js
+++ static/js/date.js
@@ -1,33 +1,35 @@
-/**
- * Date for header
- */
-function digitalDate() {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-    if (day < 10) day = "0" + day;
-    if (month < 10) month = "0" + month;
-    document.getElementById("digitalDate").innerHTML = day + "." + month + "." + year;
-    setTimeout("digitalDate()", 1000);
-}
-
-digitalDate();
-
-
-/**
- * Clock for header
- */
-function digitalWatch() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-    document.getElementById("digitalWatch").innerHTML = hours + ":" + minutes + ":" + seconds;
-    setTimeout("digitalWatch()", 1000);
-}
-
-digitalWatch();
+//
+// /**
+//  * Date for header
+//  */
+// function digitalDate() {
+//     var date = new Date();
+//     var day = date.getDate();
+//     var month = date.getMonth();
+//     var year = date.getFullYear();
+//     if (day < 10) day = "0" + day;
+//     if (month < 10) month = "0" + month;
+//     document.getElementById("digitalDate").innerHTML = day + "." + month + "." + year;
+//     setTimeout("digitalDate()", 1000);
+// }
+//
+// digitalDate();
+//
+//
+// /**
+//  * Clock for header
+//  */
+// function digitalWatch() {
+//     var date = new Date();
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     if (hours < 10) hours = "0" + hours;
+//     if (minutes < 10) minutes = "0" + minutes;
+//     if (seconds < 10) seconds = "0" + seconds;
+//     document.getElementById("digitalWatch").innerHTML = hours + ":" + minutes + ":" + seconds;
+//     setTimeout("digitalWatch()", 1000);
+// }
+//
+// digitalWatch();
+//
